9d4eccbe39e2b180942d0960a725017cac639e6c
Jacob Shore <jshore@datalign.net>
2021-02-15 00:04:08 +0200

    format file

--- src/main.rs
+++ src/main.rs
@@ -101,13 +101,13 @@ fn main() {
         let mut ans_vec: Vec<i32> = Vec::new();
         // let mut solved = false;
         for c in phrase.as_bytes().iter() {
-            match *c as char {
+            match c {
                 _ if guess_vector.contains(&c.to_ascii_lowercase()) => print!("{}", *c as char),
                 _ if *c < 65 || *c > 122 => print!("{}", *c as char),
                 _ => {
                     ans_vec.push(1);
-                    print!("_") }
-
+                    print!("_")
+                }
             }
         }
         print!("\n");
